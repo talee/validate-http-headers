06c5e92e5a195e93f399af26e679de3e211dce98
Thomas Lee <tleehoc@gmail.com>
2016-07-02 09:00:25 -0700

    Better logging and matching

--- validator.go
+++ validator.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"os"
 )
@@ -21,6 +20,15 @@ type SpecContainer struct {
 	Specs   []Spec
 }
 
+const (
+	FileError = iota
+	UnmarshalError
+	InvalidRequest
+	FailedRequest
+	MissingResponseHeader
+	FailAssertResponseHeaderValue
+)
+
 func main() {
 	var filename string
 	if len(os.Args) == 2 {
@@ -32,7 +40,7 @@ func main() {
 	file, e := ioutil.ReadFile(filename)
 	if e != nil {
 		fmt.Printf("File error: %v\n", e)
-		os.Exit(1)
+		os.Exit(FileError)
 	}
 
 	// Marshal from JSON
@@ -40,46 +48,83 @@ func main() {
 	e = json.Unmarshal(file, &specContainer)
 	if e != nil {
 		fmt.Printf("Unmarshal error: %v\n", e)
+		os.Exit(UnmarshalError)
 	}
 	specs := specContainer.Specs
 	defaultSpec := specContainer.Default
 
 	// Request each URL in specs
 	for _, spec := range specs {
-		fmt.Println("URL:", spec.Url)
-		resp, err := http.Get(spec.Url)
+		// Setup request headers
+		req, err := http.NewRequest("GET", spec.Url, nil)
 		if err != nil {
-			log.Fatalf("http.Get => %v", err.Error())
+			fmt.Printf("Failed to create request:", err)
+			os.Exit(InvalidRequest)
+		}
+		for key, headerValues := range spec.RequestHeaders {
+			for _, val := range headerValues {
+				req.Header.Add(key, val)
+			}
 		}
 
-		// Validate headers
+		// Send request
+		fmt.Println("\nURL:", req.URL)
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			fmt.Printf("http.Get => %v", err.Error())
+			os.Exit(FailedRequest)
+		}
+
+		// Validate response headers
 		expectedHeaders := clone(defaultSpec.ResponseHeaders,
 			spec.ResponseHeaders)
 
 		for key, expectedHeaderValues := range expectedHeaders {
+			// Assert exist/nonexistence
 			numResponseHeaderValues := len(resp.Header[key])
-			if numResponseHeaderValues == 0 ||
-				numResponseHeaderValues != len(expectedHeaderValues) {
-				log.Fatalf("Missing response header %v", key)
+			expectedNumHeaderValues := len(expectedHeaderValues)
+			if expectedNumHeaderValues == 1 && expectedHeaderValues[0] == "" {
+				expectedNumHeaderValues = 0
 			}
+			if numResponseHeaderValues != expectedNumHeaderValues {
+				fmt.Printf("FAIL: Mismatch of existence for response header "+
+					"'%v': Expected %v response headers with array values"+
+					"'%v'. Got header values '%v'\n",
+					key,
+					expectedNumHeaderValues,
+					expectedHeaderValues,
+					resp.Header[key])
+				continue
+			} else if expectedNumHeaderValues == 0 {
+				fmt.Println("SUCCESS: Response header '"+key+"' should not",
+					"exist")
+				continue
+			}
+
+			// Assert equality
 			for i, expectedHeaderValue := range expectedHeaderValues {
 				respHeaderValue := resp.Header[key][i]
 				if expectedHeaderValue != respHeaderValue {
-					log.Fatalf("Header assertion failed: Expected '%v'"+
-						"to have '%v' instead of '%v'", key,
+					fmt.Printf("Header assertion failed: Expected '%v'"+
+						"to have '%v' instead of '%v'\n", key,
 						expectedHeaderValue, respHeaderValue)
+				} else {
+					fmt.Println("SUCCESS: Expected", "'"+key+"'",
+						"to have", "'"+expectedHeaderValue+"'", "match",
+						"'"+respHeaderValue+"'")
 				}
 			}
 		}
 	}
 }
 
+// Returns a merge of the maps. Nil values ignored
 func clone(maps ...map[string][]string) map[string][]string {
 	result := make(map[string][]string)
 	for _, m := range maps {
-		for key, val := range m {
-			if val != nil {
-				result[key] = val
+		for key, values := range m {
+			if values != nil {
+				result[key] = values
 			}
 		}
 	}
